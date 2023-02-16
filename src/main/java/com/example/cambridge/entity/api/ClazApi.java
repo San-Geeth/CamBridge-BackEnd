fbecf264e0b58ad94b2c2f8d8df3b11e43aef89f
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2023-02-16 22:47:25 +0530

    separate getters and setters from bean

---
+++ src/main/java/com/example/cambridge/entity/api/ClazApi.java
@@ -0,0 +1,50 @@
+package com.example.cambridge.entity.api;
+
+import com.example.cambridge.entity.staff.Teacher;
+
+import java.util.Date;
+
+public interface ClazApi {
+    public Integer getId();
+
+    public void setId(Integer id);
+
+    public String getSubject();
+
+    public void setSubject(String subject);
+
+    public String getDay();
+
+    public void setDay(String day);
+
+    public Date getTimeFrom();
+
+    public void setTimeFrom(Date timeFrom);
+
+    public Date getTimeTo();
+
+    public void setTimeTo(Date timeTo);
+
+    public String getHall();
+
+    public void setHall(String hall);
+    public Teacher getTeacher();
+
+    public void setTeacher(Teacher teacher);
+
+    public Date getCreatedAt();
+
+    public void setCreatedAt(Date createdAt);
+
+    public Date getLastUpdated();
+
+    public void setLastUpdated(Date lastUpdated);
+
+    public String getStartTime();
+
+    public void setStartTime(String startTime);
+
+    public String getEndTime();
+
+    public void setEndTime(String endTime);
+}
--- src/main/java/com/example/cambridge/entity/classes/Claz.java
+++ src/main/java/com/example/cambridge/entity/classes/Claz.java
@@ -1,5 +1,6 @@
 package com.example.cambridge.entity.classes;
 
+import com.example.cambridge.entity.api.ClazApi;
 import com.example.cambridge.entity.staff.Teacher;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
@@ -14,7 +15,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "class")
-public class Claz {
+public class Claz implements ClazApi {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -36,92 +37,4 @@ public class Claz {
     private String startTime;
     @Transient
     private String endTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public Date getTimeFrom() {
-        return timeFrom;
-    }
-
-    public void setTimeFrom(Date timeFrom) {
-        this.timeFrom = timeFrom;
-    }
-
-    public Date getTimeTo() {
-        return timeTo;
-    }
-
-    public void setTimeTo(Date timeTo) {
-        this.timeTo = timeTo;
-    }
-
-    public String getHall() {
-        return hall;
-    }
-
-    public void setHall(String hall) {
-        this.hall = hall;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
 }
